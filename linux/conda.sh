6fed498a667a1b9a4f2e9cffe4cc6c47d4a8f8c6
Sidney Douw <s.douw@gmx.de>
2022-09-07 22:23:29 +0200

    update

--- linux/conda.sh
+++ linux/conda.sh
@@ -7,7 +7,10 @@ PPWD=$(pwd)
 # change the working directory to the location of this script
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-curl -o https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh | bash
+conda_version=Anaconda3-2022.05-Linux-x86_64.sh
+
+wget "https://repo.anaconda.com/archive/$conda_version"
+bash "$conda_version"
 
 # change the working directory to the previous working directory
 cd "$PPWD"
