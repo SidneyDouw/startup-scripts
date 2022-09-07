beb4bf34def500b2c55be2c3975928aeb8fc9922
Sidney Douw <s.douw@gmx.de>
2022-09-07 23:06:26 +0200

    actual fix

--- linux/util/find_rc_file.sh
+++ linux/util/find_rc_file.sh
@@ -4,6 +4,6 @@ set -xe
 
 if [ "$SHELL" == "/bin/bash" ] || [ "$SHELL" == "/usr/bin/bash" ]; then
     echo ".bashrc"
-elif [ "$SHELL" == "usr/bin/zsh" ]; then
+elif [ "$SHELL" == "/usr/bin/zsh" ]; then
     echo ".zshrc"
 fi
