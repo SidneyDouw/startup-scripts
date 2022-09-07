065215e3533ac0ca8a54bef29da245284eabe5b4
Sidney Douw <s.douw@gmx.de>
2022-09-07 16:32:48 +0200

    remove code for trying to continue script after reboot

--- startup_linux.sh
+++ startup_linux.sh
@@ -9,45 +9,5 @@ startup_scripts_folder=".startup-scripts"
 
 git clone https://github.com/SidneyDouw/startup-scripts.git "$HOME/$startup_scripts_folder" --depth 1 || true
 
-if [ ! -f /etc/init.d/startup-script ]; then
-    echo \
-"#!/bin/sh
-
-### BEGIN INIT INFO
-# Provides:          startup-script
-### END INIT INFO
-
-PATH=/sbin:/bin:/usr/sbin:/usr/bin
-
-case \$1 in
-    start)
-        $HOME/$startup_scripts_folder/startup_linux.sh
-        ;;
-    stop|restart|reload)
-        ;;
-esac" \
-| sudo tee /etc/init.d/startup-script
-
-sudo chmod +x /etc/init.d/startup-script
-fi
-
-before_reboot(){
-    cd "$HOME/$startup_scripts_folder/linux"
-    ./updates.sh
-}
-
-after_reboot(){
-    ./nvm.sh
-}
-
-if [ -f /var/run/rebooting-for-updates ]; then
-    after_reboot
-    sudo rm /var/run/rebooting-for-updates
-    sudo update-rc.d startup-script remove
-else
-    before_reboot
-    sudo touch /var/run/rebooting-for-updates
-    sudo update-rc.d startup-script defaults
-    sudo reboot now
-fi
-
+cd "$HOME/$startup_scripts_folder/linux"
+./updates.sh
