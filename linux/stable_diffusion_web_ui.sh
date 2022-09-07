1aeb3d5089fa57c638db52115e31d2dd5685fef2
Sidney Douw <s.douw@gmx.de>
2022-09-07 23:45:59 +0200

    updates

--- linux/stable_diffusion_web_ui.sh
+++ linux/stable_diffusion_web_ui.sh
@@ -2,8 +2,13 @@
 
 set -xe
 
-md "$HOME/stable_diffusion_web_ui"
+mkdir "$HOME/stable_diffusion_web_ui"
 cd "$HOME/stable_diffusion_web_ui"
+
+if [[ -z "$PW" ]]; then
+    echo "Please provie your huggingface password via the PW variable"
+    exit
+fi
 
 curl -O https://github.com/JoshuaKimsey/Linux-StableDiffusion-Script/blob/main/linux-sd.sh
 curl -L -O -u "sidney@magig.de:$PW" https://huggingface.co/CompVis/stable-diffusion-v-1-4-original/resolve/main/sd-v1-4.ckpt
