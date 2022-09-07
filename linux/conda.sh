a0d3613d68e9a14eef3291120b7d9f4c21a9ad73
Sidney Douw <s.douw@gmx.de>
2022-09-07 22:53:13 +0200

    update conda

--- linux/conda.sh
+++ linux/conda.sh
@@ -8,18 +8,19 @@ PPWD=$(pwd)
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 conda_version=Anaconda3-2022.05-Linux-x86_64.sh
+conda_path="$HOME/anaconda"
 
 wget "https://repo.anaconda.com/archive/$conda_version"
-bash "$conda_version" -b
+bash "$conda_version" -b -p "$conda_path"
 
 rm "$conda_version"
 
 rc_file=$(./util/find_rc_file.sh)
 
 echo \
-'
+"
 # Anaconda
-export PATH="$HOME/anaconda3/bin:$PATH"' \
+export PATH=\"$conda_path/bin:\$PATH\"" \
 | tee -a "$HOME/$rc_file"
 
 # change the working directory to the previous working directory
