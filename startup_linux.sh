e57d2b2d68f8cb013777e1c12bd2e31bbfbf2424
Sidney Douw <s.douw@gmx.de>
2022-09-07 17:36:17 +0200

    fix repo ownership

--- startup_linux.sh
+++ startup_linux.sh
@@ -5,6 +5,7 @@ set -xe
 startup_scripts_folder="/home/ubuntu/.startup-scripts"
 
 git clone https://github.com/SidneyDouw/startup-scripts.git "$startup_scripts_folder" --depth 1 || true
+sudo chown -R ubuntu:ubuntu .startup-scripts/
 
 cd "$startup_scripts_folder/linux"
 ./updates.sh
