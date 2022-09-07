843e1e62eb43d238176e41d439bbe11de2e4379c
Sidney Douw <s.douw@gmx.de>
2022-09-07 18:38:38 +0200

    split nvm and node

--- linux/node.sh
+++ linux/node.sh
@@ -10,20 +10,18 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 # Arguments
 
 dry_run="false"
-nvm_version=$(./util/git_latest_version.sh nvm-sh/nvm)
 node_version="node"
 
 while [[ $# -gt 0 ]]; do
     case $1 in
         -h|--help)
             echo
-            echo "Installs the latest versions of nvm and node"
+            echo "Installs the latest (or given) version node via nvm"
             echo 
             echo "Usage:"
             echo
             echo "  -h|--help         Prints this message."
             echo "  -d|--dry-run      Runs the script without installing anything."
-            echo "  --nvm             Version of nvm to use, e.g. 'v0.39.1' or '--lts'. Defaults to the latest version."
             echo "  --node            Version of node to use, e.g. 'v16.17.0'. Defaults to the latest version."
             exit
             ;;
@@ -31,11 +29,6 @@ while [[ $# -gt 0 ]]; do
             dry_run="true"
             shift
             ./util/prepare_dryrun.sh
-            ;;
-        --nvm)
-            nvm_version=$2
-            shift
-            shift
             ;;
         --node)
             node_version=$2
@@ -48,19 +41,6 @@ while [[ $# -gt 0 ]]; do
             ;;
     esac
 done
-
-# Install NVM
-
-echo "Installing NVM version $nvm_version to $HOME/.nvm"
-
-if [[ $dry_run == "false" ]]; then
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$nvm_version/install.sh | bash
-
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-fi
-
 
 # Install Node
 
