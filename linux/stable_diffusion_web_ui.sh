55031ce5603fd709a717fb17aca88692fe0ee55c
Sidney Douw <s.douw@gmx.de>
2022-09-07 23:49:44 +0200

    updates

--- linux/stable_diffusion_web_ui.sh
+++ linux/stable_diffusion_web_ui.sh
@@ -10,7 +10,7 @@ fi
 mkdir "$HOME/stable_diffusion_web_ui"
 cd "$HOME/stable_diffusion_web_ui"
 
-curl -O https://github.com/JoshuaKimsey/Linux-StableDiffusion-Script/blob/main/linux-sd.sh
+curl -O https://raw.githubusercontent.com/JoshuaKimsey/Linux-StableDiffusion-Script/main/linux-sd.sh
 curl -L -O -u "sidney@magig.de:$PW" https://huggingface.co/CompVis/stable-diffusion-v-1-4-original/resolve/main/sd-v1-4.ckpt
 
 chmod +x linux-sd.sh
