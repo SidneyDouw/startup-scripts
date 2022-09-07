1ac4b1779ad1a84cb3ca10d7676aabbd1f7d176a
Sidney Douw <s.douw@gmx.de>
2022-09-07 22:27:03 +0200

    fix

--- linux/conda.sh
+++ linux/conda.sh
@@ -10,7 +10,9 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 conda_version=Anaconda3-2022.05-Linux-x86_64.sh
 
 wget "https://repo.anaconda.com/archive/$conda_version"
-bash "$conda_version"
+bash "$conda_version" -b
+
+rm "$conda_version"
 
 # change the working directory to the previous working directory
 cd "$PPWD"
