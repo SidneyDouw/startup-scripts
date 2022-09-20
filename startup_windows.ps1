53c7921843bb0717c429fb18382f32f417e6391f
Sidney Douw <s.douw@gmx.de>
2022-09-20 13:32:28 +0200

    windows updates

--- startup_windows.ps1
+++ startup_windows.ps1
@@ -1,11 +1,6 @@
 $startup_scripts_folder = "C:\\.startup-scripts"
 
 git clone https://github.com/SidneyDouw/startup-scripts.git "$startup_scripts_folder" --depth 1
-# sudo chown -R ubuntu:ubuntu "$startup_scripts_folder"
-
 Set-Location "$startup_scripts_folder/windows"
-# ./updates.sh
 
-./test.ps1
-
-# sudo reboot
+./chocolatey.ps1
