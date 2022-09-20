cfe58b14252c44943cc4e9d4c4e9fcc540af0a8c
Sidney Douw <s.douw@gmx.de>
2022-09-20 14:31:32 +0200

    updates

--- startup_windows.ps1
+++ startup_windows.ps1
@@ -1,6 +1,15 @@
-$startup_scripts_folder = "C:\\.startup-scripts"
+<powershell>
 
+# Install chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco install -y git
+choco install -y vcredist140
+
+$startup_scripts_folder = "C:\\.startup-scripts"
 git clone https://github.com/SidneyDouw/startup-scripts.git "$startup_scripts_folder" --depth 1
 Set-Location "$startup_scripts_folder/windows"
 
-./chocolatey.ps1
+</powershell>
---
+++ windows/nvidia-drivers.ps1
@@ -0,0 +1,11 @@
+$Bucket = "ec2-windows-nvidia-drivers"
+$KeyPrefix = "latest"
+$LocalPath = "$home\Desktop\NVIDIA"
+$Objects = Get-S3Object -BucketName $Bucket -KeyPrefix $KeyPrefix -Region us-east-1
+foreach ($Object in $Objects) {
+    $LocalFileName = $Object.Key
+    if ($LocalFileName -ne '' -and $Object.Size -ne 0) {
+        $LocalFilePath = Join-Path $LocalPath $LocalFileName
+        Copy-S3Object -BucketName $Bucket -Key $Object.Key -LocalFile $LocalFilePath -Region us-east-1
+    }
+}
