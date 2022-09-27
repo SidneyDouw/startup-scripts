49016cea187fee2b24718672861af90fe98e31cd
Sidney Douw <s.douw@gmx.de>
2022-09-27 15:05:32 +0200

    update pw

--- windows/add-user.ps1
+++ windows/add-user.ps1
@@ -1,6 +1,6 @@
 # Add new local user, add him to Administrators
 $username = "user"
-$password = "password"
+$password = "Password#"
 
 New-LocalUser $username -Password (ConvertTo-SecureString $password -AsPlainText -Force)
 Set-LocalUser -Name $username -PasswordNeverExpires 1
