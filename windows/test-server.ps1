04abf77459a9c4bbedd4abec51e1f7a538c4cf1c
Sidney Douw <s.douw@gmx.de>
2022-09-27 14:32:32 +0200

    windows test server

---
+++ windows/test-server.ps1
@@ -0,0 +1,16 @@
+npm install -g pm2
+npm install -g pm2-windows-startup
+pm2-startup install
+
+Set-Content -Path 'C:\server.js' -Value "var http = require('http');
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<html><body><p>Server is running</p></body></html>');
+    res.end();
+});
+server.listen(8080)
+console.log('Node.js web server at port 8080 is running..')
+"
+
+pm2 start "C:\server.js"
+pm2 save
