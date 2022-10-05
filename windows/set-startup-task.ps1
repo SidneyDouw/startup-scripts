efdb620a17282ccbacbfc6d0762f03209a33418d
Sidney Douw <s.douw@gmx.de>
2022-10-05 18:15:10 +0200

    fix set-startup-task script

--- windows/set-startup-task.ps1
+++ windows/set-startup-task.ps1
@@ -3,4 +3,4 @@ param ( [string]$Name, [string]$Exe, [string]$Argument)
 $action = New-ScheduledTaskAction -Execute $Exe -Argument $Argument
 $trigger =  New-ScheduledTaskTrigger -AtStartup
 
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $Name -Description ""
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $Name # -Description ""
