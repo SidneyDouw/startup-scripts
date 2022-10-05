1f8402c243cafcc29dfc708d568b33868d86b8a9
Sidney Douw <s.douw@gmx.de>
2022-10-05 18:27:24 +0200

    fix permissions for startup script

--- windows/set-startup-task.ps1
+++ windows/set-startup-task.ps1
@@ -2,5 +2,6 @@ param ( [string]$Name, [string]$Exe, [string]$Argument)
 
 $action = New-ScheduledTaskAction -Execute $Exe -Argument $Argument
 $trigger =  New-ScheduledTaskTrigger -AtStartup
+$principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
 
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $Name # -Description ""
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $Name -Principal $principal
