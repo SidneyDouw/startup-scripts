707107373a0845cbf73e5e70753e774c2a17f17e
Sidney Douw <s.douw@gmx.de>
2022-10-05 17:41:21 +0200

    added set-startup-task script

---
+++ windows/set-startup-task.ps1
@@ -0,0 +1,6 @@
+param ( [string]$Name, [string]$Exe, [string]$Argument)
+
+$action = New-ScheduledTaskAction -Execute $Exe -Argument $Argument
+$trigger =  New-ScheduledTaskTrigger -AtStartup
+
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $Name -Description ""
