4fd84c75261680b7c60f0655e1d922449393fecb
Sidney Douw <s.douw@gmx.de>
2023-02-27 18:10:41 +0000

    add set-logon-task script

---
+++ windows/set-logon-task.ps1
@@ -0,0 +1,7 @@
+param ( [string]$Name, [string]$Exe, [string]$Argument)
+
+$action = New-ScheduledTaskAction -Execute $Exe -Argument $Argument
+$trigger =  New-ScheduledTaskTrigger -AtLogOn
+$principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $Name -Principal $principal
