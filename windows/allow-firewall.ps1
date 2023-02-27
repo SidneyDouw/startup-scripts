76d64906efd7f586580623e29e4e0764a323e983
Sidney Douw <s.douw@gmx.de>
2023-02-27 22:50:42 +0000

    add firewall-allow-program script

--- windows/allow-firewall.ps1
+++
@@ -1,3 +0,0 @@
-param ( [int]$Port, [string]$Protocol)
-
-New-NetFirewallRule -DisplayName "ALLOW $Protocol PORT $Port" -Direction inbound -Profile Any -Action Allow -LocalPort $Port -Protocol $Protocol
---
+++ windows/firewall-allow-program.ps1
@@ -0,0 +1,4 @@
+param ( [int]$Program )
+
+$program_name = Split-Path -Path -$Program -Leaf
+New-NetFirewallRule -DisplayName $program_name -Direction Inbound -Program $Program -Action Allow
