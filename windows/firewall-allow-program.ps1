b426e4af288de0bc5f1bdf2d038ad68b1b3be7c5
Sidney Douw <s.douw@gmx.de>
2023-02-27 23:12:23 +0000

    fix parameter type

--- windows/firewall-allow-program.ps1
+++ windows/firewall-allow-program.ps1
@@ -1,4 +1,4 @@
-param ( [int]$Program )
+param ( [string]$Program )
 
 $program_name = Split-Path -Path -$Program -Leaf
 New-NetFirewallRule -DisplayName $program_name -Direction Inbound -Program $Program -Action Allow
