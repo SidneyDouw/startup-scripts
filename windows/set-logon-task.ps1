b8e28af1282d60fbed4e137c37e299a22df6c3e1
Sidney Douw <s.douw@gmx.de>
2023-02-27 18:38:32 +0000

    update logon-task script

--- windows/set-logon-task.ps1
+++ windows/set-logon-task.ps1
@@ -1,7 +1,9 @@
-param ( [string]$Name, [string]$Exe, [string]$Argument)
+param ( [string]$Name, [string] $Username ,[string]$Exe, [string]$Argument)
+
+$domain_name = (Get-WmiObject Win32_Computersystem).name
 
 $action = New-ScheduledTaskAction -Execute $Exe -Argument $Argument
 $trigger =  New-ScheduledTaskTrigger -AtLogOn
-$principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+$principal = New-ScheduledTaskPrincipal -UserID "$domain_name\$Username" -LogonType ServiceAccount -RunLevel Highest
 
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $Name -Principal $principal
