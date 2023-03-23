31dba19866fc991bf70921ead6b2046c12289fe7
Sidney Douw <mail@sidneydouw.de>
2023-03-23 16:08:28 +0100

    Update add-user.ps1
    revert

--- windows/add-user.ps1
+++ windows/add-user.ps1
@@ -7,6 +7,3 @@ $secure_password = ConvertTo-SecureString $password -AsPlainText -Force
 New-LocalUser $username -Password $secure_password
 Set-LocalUser -Name $username -PasswordNeverExpires 1
 Add-LocalGroupMember -Group "Administrators" -Member $username
-
-$cred = New-Object System.Management.Automation.PSCredential -ArgumentList $username,$secure_password
-Start-Process cmd /c -WindowStyle Hidden -Credential $cred -ErrorAction SilentlyContinue
