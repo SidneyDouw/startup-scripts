346274ebab1f1777c2fa9b2f2e24b5dab35c2fcf
Sidney Douw <mail@sidneydouw.de>
2023-03-23 15:16:03 +0100

    Update add-user.ps1
    when creating a new user, set its home directory

--- windows/add-user.ps1
+++ windows/add-user.ps1
@@ -1,6 +1,9 @@
 # Add new local user, add him to Administrators
 param ( [string]$username, [string]$password)
 
+$home_dir = "C:\Users\$username"
+
 New-LocalUser $username -Password (ConvertTo-SecureString $password -AsPlainText -Force)
-Set-LocalUser -Name $username -PasswordNeverExpires 1
+New-Item -Path $home_dir -ItemType Directory
+Set-LocalUser -Name $username -PasswordNeverExpires 1 -HomeDirectory $home_dir
 Add-LocalGroupMember -Group "Administrators" -Member $username
