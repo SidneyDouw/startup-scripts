8f3104ca491d693cd80187035bb45ffbfa6c6b73
Sidney Douw <mail@sidneydouw.de>
2023-03-23 15:49:51 +0100

    Update add-user.ps1
    try to add home folder automatically

--- windows/add-user.ps1
+++ windows/add-user.ps1
@@ -2,8 +2,11 @@
 param ( [string]$username, [string]$password)
 
 $home_dir = "C:\Users\$username"
+$secure_password = ConvertTo-SecureString $password -AsPlainText -Force
 
-New-LocalUser $username -Password (ConvertTo-SecureString $password -AsPlainText -Force)
-New-Item -Path $home_dir -ItemType Directory
+New-LocalUser $username -Password $secure_password
 Set-LocalUser -Name $username -PasswordNeverExpires 1
 Add-LocalGroupMember -Group "Administrators" -Member $username
+
+$cred = New-Object System.Management.Automation.PSCredential -ArgumentList $username,$secure_password
+Start-Process cmd /c -WindowStyle Hidden -Credential $cred -ErrorAction SilentlyContinue
