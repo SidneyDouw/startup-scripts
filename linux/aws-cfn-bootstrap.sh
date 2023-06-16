e76207085b23a42994b8571498cdd5b6a61a963f
Sidney Douw <mail@sidneydouw.de>
2023-06-16 12:06:47 +0200

    some fixes

--- linux/aws-cfn-bootstrap.sh
+++ linux/aws-cfn-bootstrap.sh
@@ -10,8 +10,10 @@ wget https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-py3-late
 tar -zxvf aws-cfn-bootstrap-py3-latest.tar.gz
 rm aws-cfn-bootstrap-py3-latest.tar.gz
 
+sudo apt -y install python3 python3-pip
+
 cd aws-cfn-bootstrap-*
-sudo easy_install .
+sudo pip install .
 
 sudo ln -s /root/aws-cfn-bootstrap-latest/init/ubuntu/cfn-hup /etc/init.d/cfn-hup
 
