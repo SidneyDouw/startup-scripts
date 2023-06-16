c5fd4a59ec39cecbab55960ea72c88a54e768ec9
Sidney Douw <mail@sidneydouw.de>
2023-06-16 11:26:27 +0200

    add aws-cfn-bootstrap script for linux

---
+++ linux/aws-cfn-bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -xe
+
+PPWD=$(pwd)
+
+cd "$HOME"
+
+wget https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-py3-latest.tar.gz
+tar -zxvf aws-cfn-bootstrap-py3-latest.tar.gz
+
+cd aws-cfn-bootstrap-*
+sudo easy_install .
+
+sudo ln -s /root/aws-cfn-bootstrap-latest/init/ubuntu/cfn-hup /etc/init.d/cfn-hup
+
+# change the working directory to the previous working directory
+cd "$PPWD"
