e07e1584f42ff3ae0e829be17ee82c77e3ef147a
Sidney Douw <mail@sidneydouw.de>
2024-02-21 09:23:59 +0000

    add newline before .rc file entry

--- linux/mise.sh
+++ linux/mise.sh
@@ -6,9 +6,11 @@ curl https://mise.run | sh
 
 # check if .zshrc file exists
 if [ -f ~/.zshrc ]; then
+    echo '' >> ~/.zshrc
     echo '# mise setup' >> ~/.zshrc
     echo 'eval "$(~/.local/bin/mise activate zsh)"' >> ~/.zshrc
 else
+    echo '' >> ~/.zshrc
     echo '# mise setup' >> ~/.bashrc
     echo 'eval "$(~/.local/bin/mise activate bash)"' >> ~/.bashrc
 fi
